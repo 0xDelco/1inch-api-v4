f0f04cfc007a4bdc49dbf86355797d150ba85d5c
0xdelco <0xdelco@protonmail.com>
2022-03-12 14:53:21 -0500

    Additional chain support and resolving issue #1.
    - Added support for Gnosis and Avax.
    - Resolving issues #1

--- src/OneInch.Api/Configuration/Abstractions/IOneInchSettings.cs
+++ src/OneInch.Api/Configuration/Abstractions/IOneInchSettings.cs
@@ -6,5 +6,8 @@ namespace OneInch.Api
         BlockchainSettings BinanceSmartChain {get;set;}
         BlockchainSettings Polygon {get;set;}
         BlockchainSettings Optimism {get;set;}
+        BlockchainSettings Arbitrum {get;set;}
+        BlockchainSettings Gnosis {get;set;}
+        BlockchainSettings Avalanche {get;set;}
     }
 }
